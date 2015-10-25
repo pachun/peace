a8518061fa00f6dda9846094a3d780643665a961
Nick Pachulski <nick@pachulski.me>
2015-10-24 22:14:14 -0400

    Add coveralls coverage to the repository.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'peace'
+require "coveralls"
+Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "peace"
